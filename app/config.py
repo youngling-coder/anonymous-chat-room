e90f4d6d85ed9e25484c8e97e7c8587c316b61cf
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-06-19 17:49:33 +0200

    comment currently unnecessary fields

--- app/config.py
+++ app/config.py
@@ -7,10 +7,9 @@ class Settings(BaseSettings):
     db_host: str
     db_port: str
     db_name: str
-    jwt_secret_key: str
-    jwt_algo: str
-    jwt_expire_minutes: int
-
+    # jwt_secret_key: str
+    # jwt_algo: str
+    # jwt_expire_minutes: int
 
     class Config:
         env_file = ".env"
