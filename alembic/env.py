edc548e89657f66f3ef44bdb4cc18c9c7bbb87e1
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-06-19 17:48:07 +0200

    set database URL for alembic

--- alembic/env.py
+++ alembic/env.py
@@ -11,7 +11,10 @@ from app.config import settings
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-config.set_main_option("sqlalchemy.url", f"postgresql://{settings.db_usr}:{settings.db_pwd}@{settings.db_host}:{settings.db_port}/{settings.db_name}")
+config.set_main_option(
+    "sqlalchemy.url",
+    f"postgresql://{settings.db_usr}:{settings.db_pwd}@{settings.db_host}:{settings.db_port}/{settings.db_name}",
+)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -68,9 +71,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
