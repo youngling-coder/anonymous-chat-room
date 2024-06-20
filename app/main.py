157648d47737c08bbf6d6bdf37accbd8533f78e4
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-06-20 15:21:46 +0200

    include authentication router to the main app

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
-from .routers import user, message
+from .routers import user, message, auth
+
 
 app = FastAPI()
 
-app.include_router(user.router, prefix="/user")
-app.include_router(message.router, prefix="/message")
+app.include_router(user.router, prefix="/users")
+app.include_router(message.router, prefix="/messages")
+app.include_router(auth.router, prefix="/auth")
