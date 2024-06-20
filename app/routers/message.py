f4e575a19c44d60ef708573d74961c205042b289
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-06-20 17:39:18 +0200

    add new endpoints to the routers

--- app/routers/message.py
+++ app/routers/message.py
@@ -1,14 +1,25 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from ..database import get_db
-from .. import schemas
+from .. import schemas, models, oauth2
 
 
 router = APIRouter(tags=["Messages"])
 
 
 @router.post(
-    "/", status_code=status.HTTP_201_CREATED, response_model=schemas.MessageResponse
+    "/create", status_code=status.HTTP_201_CREATED, response_model=schemas.MessageResponse
 )
-def create_post(message: schemas.CreateMessage, db: Session = Depends(get_db)):
-    pass
+def create_message(
+    message: schemas.CreateMessage,
+    db: Session = Depends(get_db),
+    current_user=Depends(oauth2.get_current_user),
+):
+
+    new_message = models.Message(owner_id=current_user.id, **message.model_dump())
+
+    db.add(new_message)
+    db.commit()
+    db.refresh(new_message)
+
+    return new_message
--- app/routers/user.py
+++ app/routers/user.py
@@ -1,14 +1,38 @@
 from fastapi import APIRouter, Depends, status, HTTPException
+import psycopg2
+from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 from ..database import get_db
-from .. import schemas
+from .. import schemas, utils, models
 
 
 router = APIRouter(tags=["Users"])
 
 
 @router.post(
-    "/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserResponse
+    "/create", status_code=status.HTTP_201_CREATED, response_model=schemas.UserResponse
 )
-def create_post(message: schemas.CreateUser, db: Session = Depends(get_db)):
-    pass
+def create_user(user: schemas.CreateUser, db: Session = Depends(get_db)):
+
+    try:
+        user.password = utils.hash(user.password)
+        new_user = models.User(**user.model_dump())
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+
+        return new_user
+    
+    except IntegrityError as e:
+        db.rollback()
+        if isinstance(e.orig, psycopg2.errors.UniqueViolation):
+            raise HTTPException(
+                status_code=status.HTTP_409_CONFLICT,
+                detail="User with such username already exists!"
+            )
+        else:
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Internal Server Error"
+            )
+
