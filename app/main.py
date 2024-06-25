9f1288346bda86cf390d343dffbe3ee4342ea6cb
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-06-25 18:35:23 +0200

    mount directory for the static files

--- app/main.py
+++ app/main.py
@@ -1,9 +1,15 @@
+from pathlib import Path
+
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from .routers import user, message, auth, template
 
+BASE_DIR = Path(__file__).resolve().parent
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory=f"{BASE_DIR}/static"), name="static")
 
 app.include_router(user.router, prefix="/users")
 app.include_router(message.router, prefix="/messages")
