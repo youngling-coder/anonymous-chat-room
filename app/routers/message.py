f6d23be4691828a3a8c741c8d3cf1ff4282c021b
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-06-27 18:02:25 +0200

    add get_latest endpoint to fetch new messages after a time point

--- app/routers/message.py
+++ app/routers/message.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter, Depends, status, HTTPException
+from datetime import datetime
+
+from fastapi import APIRouter, Depends, status, Response
 from sqlalchemy.orm import Session
 from ..database import get_db
 from .. import schemas, models, oauth2
@@ -25,3 +27,16 @@ def create_message(
     db.refresh(new_message)
 
     return new_message
+
+
+@router.get("/get_latest", response_model=list[schemas.MessageResponse])
+def get_latest_messages(timestamp: str, db: Session = Depends(get_db), current_user=Depends(oauth2.get_current_user)):
+
+    from_timestamp = datetime.fromisoformat(timestamp.replace("Z", "+00:00"))
+
+    messages = db.query(models.Message).filter(models.Message.timestamp >= from_timestamp).all()
+
+    if not messages:
+        return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+    return messages
