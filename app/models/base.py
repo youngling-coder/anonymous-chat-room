a618d800c5ed91179fc29d1f862c9fa43e945fa1
Dmytro Shyrokov <shyrokov.dmytro@proton.me>
2024-06-27 18:00:28 +0200

    add timestamp column to the Base database model

--- app/models/base.py
+++ app/models/base.py
@@ -1,5 +1,6 @@
+from datetime import datetime, UTC
 from sqlalchemy.orm import DeclarativeBase, declared_attr, Mapped, mapped_column
-
+from sqlalchemy import func
 
 class Base(DeclarativeBase):
     __abstract__ = True
@@ -9,6 +10,10 @@ class Base(DeclarativeBase):
         return f"{cls.__name__.lower()}s"
 
     id: Mapped[int] = mapped_column(primary_key=True)
+    timestamp: Mapped[datetime] = mapped_column(
+        default=lambda: datetime.now(UTC),
+        server_default=func.now()
+    )
 
 
 base = Base()
